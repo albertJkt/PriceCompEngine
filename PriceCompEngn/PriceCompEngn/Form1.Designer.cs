9548eca3f22c17a3d83a32d0439b7f3e076abba3
Devel420 <rytis.bankieta@gmail.com>
2017-09-11 21:14:04 +0300

    New test commit
    bbd blet gal nx dabar jau veiks

--- PriceCompEngn/PriceCompEngn/Form1.Designer.cs
+++ PriceCompEngn/PriceCompEngn/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -40,11 +41,21 @@
             this.textBox1.Text = "Hello darkness my old friend";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 72);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
