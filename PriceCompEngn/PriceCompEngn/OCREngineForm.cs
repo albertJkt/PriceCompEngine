b74c6b192760ce82f937331bc9fabe531c273de1
Rytis Bankieta <rytis.bankieta@gmail.com>
2017-09-24 20:25:07 +0300

    Add image text recognition using Google Vision API

---
+++ PriceCompEngn/PriceCompEngn/OCREngineForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/// <summary>
+/// This is the summary
+/// </summary>
+
+namespace PriceCompEngn
+{
+    public partial class OCREngineForm : Form
+    {
+        public string ImagePath { get; set; }
+        public OCREngineForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void upload_button_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                read_button.Enabled = false;
+                dialog.Filter = "jpeg files|*.jpg;*.JPG";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    FileInfo fileInfo = new FileInfo(dialog.FileName);
+                    if (fileInfo.Length > 1024 * 1024)
+                    {
+                        MessageBox.Show("File too large");
+                        return;
+                    }
+                    uploaded_image.Image = Image.FromFile(dialog.FileName);
+                    ImagePath = dialog.FileName;
+                    read_button.Enabled = true;
+                }
+            }
+        }
+
+
+        private string ResultTextString = "";
+        private void read_button_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ImagePath))
+                return;
+
+
+            converted_text.Text = "";
+            GoogleAnnotate annotate = new GoogleAnnotate();
+            annotate.GetText(ImagePath, "lt");
+            if (string.IsNullOrEmpty(annotate.Error) == false)
+                MessageBox.Show("Error" + annotate.Error);
+            else
+            {
+                ResultTextString = annotate.TextResult;
+                converted_text.Text = annotate.TextResult;
+            }
+        }
+    }
+}
