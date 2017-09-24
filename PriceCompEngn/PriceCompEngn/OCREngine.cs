b9ecb232e20c581d9fbce62ebcd96325f9683237
Albert Jurkoit <31667609+albertJkt@users.noreply.github.com>
2017-09-24 13:37:47 +0300

    Add OCR engine Tesseract, test it with my image

---
+++ PriceCompEngn/PriceCompEngn/OCREngine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+
+namespace PriceCompEngn
+{
+    public partial class OCREngine : Form
+    {
+        public OCREngine()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btOCR_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var image = new Bitmap(openFileDialog.FileName);
+                var ocr = new TesseractEngine("./tessdata", "lit", EngineMode.Default);
+                var page = ocr.Process(image);
+                txtBox.Text = page.GetText();
+            }
+        }
+    }
+}
