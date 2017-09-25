d49591b1dbd85d3dd63d281fd9399f9f779b33e1
Albert Jurkoit <31667609+albertJkt@users.noreply.github.com>
2017-09-25 19:06:11 +0300

    Unit test test

---
+++ PriceCompEngn/UnitTestProject/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject
+{
+    public class Calculator
+    {
+        internal object Sum(int v1, int v2)
+        {
+            //throw new NotImplementedException();
+            return v1 + v2;
+        }
+    }
+}
---
+++ PriceCompEngn/UnitTestProject/CalculatorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Is2Plus2Equal4()
+        {
+            //Arrane
+            var Calculator = new Calculator();
+            //Act
+            var sum = Calculator.Sum(2, 2);
+            //Assert
+            Assert.AreEqual(4, sum);
+
+        }
+    }
+}
+
+
+/// 3 dalys testo
+/// A. Access / Arrange
+/// B. Act
+/// C. Assert
