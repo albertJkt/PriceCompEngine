ba5f432370c67ca3523fa9f3aa282a8849f4a0b2
Albert Jurkoit <31667609+albertJkt@users.noreply.github.com>
2017-10-01 13:43:52 +0300

    Add database project, create online database

---
+++ PriceCompEngn/DataBase/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private const string ConnectionString = "Server=tcp:pricecompengn.database.windows.net,1433;Initial Catalog=PriceCompEngine;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;0";
+    }
+}
