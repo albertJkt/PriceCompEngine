0ff9c0f2fad79fbf0e4c2f7e5076417af455ab88
KaSaras <sarunas.kazimieras.buteikis@gmail.com>
2017-10-08 14:59:34 +0300

    Fix bug
    fixed a compile error. Simply had to change project type and version

---
+++ PriceCompEngn/LogicTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LogicTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LogicTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("05f0eee4-41d1-44fc-9080-fb0760019a6f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PriceCompEngn/LogicTests/ShopGoodsTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+
+namespace LogicTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Top5GoodsTest()
+        {
+            //arrange
+            ShopGoods c = new ShopGoods();
+
+            //act
+            c.Top5Goods();
+            //assert
+            Assert.Fail();
+        }
+    }
+}
