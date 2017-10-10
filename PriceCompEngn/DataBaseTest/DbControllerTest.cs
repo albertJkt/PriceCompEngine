fd19f720e8593af589c29160d610ca5f2b1756e9
Rytis Bankieta <rytis.bankieta@gmail.com>
2017-10-11 00:40:43 +0300

    Remove unnecessary code

--- PriceCompEngn/DataBaseTest/DbControllerTest.cs
+++ PriceCompEngn/DataBaseTest/DbControllerTest.cs
@@ -10,26 +10,6 @@ namespace DataBaseTest
     public class DBControllerTest
     {
         [TestMethod]
-        public void GetProductsTest()
-        {
-            //arrange
-            DBController controller = new DBController();
-            string type = "vanduo";
-            string[] shops =
-            {
-                "Maxima",
-                "Iki",
-                "Rimi",
-                "Norfa"
-            };
-            int days = 14;
-            //act
-            List<Product> retrvievedProducts = controller.GetProducts(type, shops, days);
-            //assert
-            Assert.IsNotNull(retrvievedProducts);
-        }
-
-        [TestMethod]
         public void GetProductsLinqTest()
         {
             DBController controller = new DBController();
