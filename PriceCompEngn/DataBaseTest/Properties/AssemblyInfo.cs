65c8fe715acbf06e7eaa16c2cad284931b80eabd
Rytis Bankieta <31857639+RytisBnk@users.noreply.github.com>
2017-10-14 17:01:17 +0300

    Add Entity framework and some basic db functions (#21)
    * Add information retrieval functionality and it's unit test
    
    > New class: DBController
    > New method: DBController.GetProducts()
    > New Unit Test project: DataBaseTest
    > Added unit test for new database project class methods
    
    * Duplicate previous commit functionality, this time using the Entity Framework
    
    It's more effective ;)
    
    * Minor changes

---
+++ PriceCompEngn/DataBaseTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataBaseTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataBaseTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("62fee92f-e6e3-43fd-b1c8-1ba6c5a645f8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
