3f3b4c1aae6f27acb332c99f4616fe288c0026dd
Rytis Bankieta <rytis.bankieta@gmail.com>
2017-10-15 21:58:06 +0300

    Add 3 different methods comparing prices
    >Get cheapest variant for a particular shop item
    >Get a list of defined length of cheapest shop items ordered from cheapest to most expensive
    >Get a lis of defined length of cheapest items of a particular item type

--- PriceCompEngn/Logic/PriceComparator.cs
+++ PriceCompEngn/Logic/PriceComparator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataBase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,44 @@ namespace Logic
 {
     class PriceComparator
     {
+        public ShopItem GetCheapestItem(string itemName, string[] shops)
+        {
+            List<ShopItem> items = new List<ShopItem>();
+            DBController db = new DBController();
 
+            foreach(string shop in shops)
+            {
+                items.Add(db.GetLatestEntry(itemName, shop));
+            }
+
+            return (from item in items
+                    orderby item.Price ascending
+                    select item).First<ShopItem>();
+        }
+
+        public List<ShopItem> GetCheapestItemList(string itemName, string[] shops, int topPlaces)
+        {
+            List<ShopItem> items = new List<ShopItem>();
+            DBController db = new DBController();
+
+            foreach (string shop in shops)
+            {
+                items.Add(db.GetLatestEntry(itemName, shop));
+            }
+
+            var sort = from item in items
+                       orderby item.Price ascending
+                       select item;
+            return sort.ToList<ShopItem>();
+        }
+
+        public List<ShopItem> GetCheapestItemTypeList(string itemType, string[] shops, int topPlaces)
+        {
+            List<ShopItem> allItems = (new DBController()).GetShopItemsList(itemType, shops, 14);
+
+            var filteredItems = allItems.GroupBy(item => item.ShopName).Select(group => group.First()).ToList<ShopItem>();
+
+            return filteredItems;
+        }
     }
 }
