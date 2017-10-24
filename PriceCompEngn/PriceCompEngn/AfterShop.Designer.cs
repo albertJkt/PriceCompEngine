58a8e5990e2a040e49114e6cd6b2b37dbbca0d4e
Albert Jurkoit <31667609+albertJkt@users.noreply.github.com>
2017-10-24 20:20:46 +0300

    UI fix, add image preview and text edit (#39)
    * Add UI
    
    * Fix UI to not throw exceptions on certain methods
    
    * fix gps
    
    * Add UI (#37) (#38)
    
    * Add UI
    
    * Fix UI to not throw exceptions on certain methods
    
    * fix gps
    
    * Update AfterShop UI, add preview and edit
    
    * Revert "Merge branch 'master' into UI"
    
    This reverts commit e018df8cd6e2d09a1b961c5cb1b723f0225b4765, reversing
    changes made to a50bf29753599bae7d69168ef03ade5f4d7fb10e.

--- PriceCompEngn/PriceCompEngn/AfterShop.Designer.cs
+++ PriceCompEngn/PriceCompEngn/AfterShop.Designer.cs
@@ -32,6 +32,10 @@
             this.btnAddImage = new System.Windows.Forms.Button();
             this.bntUpload = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -56,7 +60,7 @@
             // 
             // bntUpload
             // 
-            this.bntUpload.Location = new System.Drawing.Point(15, 388);
+            this.bntUpload.Location = new System.Drawing.Point(15, 464);
             this.bntUpload.Name = "bntUpload";
             this.bntUpload.Size = new System.Drawing.Size(428, 23);
             this.bntUpload.TabIndex = 2;
@@ -68,25 +72,54 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Enabled = false;
-            this.label2.Location = new System.Drawing.Point(179, 440);
+            this.label2.Location = new System.Drawing.Point(114, 505);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(152, 13);
+            this.label2.Size = new System.Drawing.Size(264, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Duomenys sėkmingai patalpinti";
+            this.label2.Text = "Kvito informacija sėkmingai patalpinta duomenų bazėje";
             this.label2.Visible = false;
             this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(15, 129);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 274);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(244, 129);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 274);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 426);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Redaguoti nuskaitytas prekės";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // AfterShop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(501, 501);
+            this.ClientSize = new System.Drawing.Size(501, 536);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.bntUpload);
             this.Controls.Add(this.btnAddImage);
             this.Controls.Add(this.label1);
             this.Name = "AfterShop";
             this.Text = "AfterShop";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,5 +131,8 @@
         private System.Windows.Forms.Button btnAddImage;
         private System.Windows.Forms.Button bntUpload;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
