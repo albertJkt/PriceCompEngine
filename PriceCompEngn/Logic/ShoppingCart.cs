5062c3394826189dd97c4752c7d48e8f48de958d
Rytis Bankieta <31857639+RytisBnk@users.noreply.github.com>
2017-10-29 20:15:22 +0200

    Shop cart (#43)
    * Add ShoppingCart class and it's unit test
    
    * Link the logic part of the functionality to the UI

---
+++ PriceCompEngn/Logic/ShoppingCart.cs
@@ -0,0 +1,59 @@
+﻿using DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class ShoppingCart
+    {
+        public string BestShop { get; private set; }
+        public float AveragePrice { get; private set; }
+        public float LowestPrice { get; private set; }
+
+        private List<string> _itemNames;
+        private string[] _shops;
+        private List<ShopItem> _bestPricedItems;
+        private Dictionary<string, float> _shopPrices;
+
+        public ShoppingCart(List<string> itemNames, string[] shops)
+        {
+            _itemNames = itemNames;
+            _shops = shops;
+            _bestPricedItems = new List<ShopItem>();
+            _shopPrices = new Dictionary<string, float>();
+            AnalyzeShoppingCart();
+        }
+
+        private void AnalyzeShoppingCart()
+        {
+            PriceComparator comparator = new PriceComparator();
+
+            foreach(string itemName in _itemNames)
+            {
+                _bestPricedItems.AddRange(comparator.GetCheapestItemList(itemName, _shops, _shops.Length));
+            }
+
+
+            foreach (string shop in _shops)
+            {
+                _shopPrices.Add(shop, GetPriceInShop(shop));
+            }
+
+            LowestPrice = _shopPrices.Values.Min();
+            AveragePrice = (float)Math.Round((decimal)_shopPrices.Values.Average(),2);
+            BestShop = _shopPrices.Where(shop => shop.Value == LowestPrice).Select(shop => shop.Key).First();
+        }
+
+        private float GetPriceInShop(string shopName)
+        {
+            var itemPrices = _bestPricedItems.Where(item => item.ShopName == shopName).Select(item => item.Price);
+
+            float sum = itemPrices.Sum();
+
+            return sum;
+        }
+    }
+}
--- PriceCompEngn/PriceCompEngn/BeforeShop.Designer.cs
+++ PriceCompEngn/PriceCompEngn/BeforeShop.Designer.cs
@@ -183,7 +183,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(614, 735);
+            this.ClientSize = new System.Drawing.Size(615, 289);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.listView3);
             this.Controls.Add(this.listView2);
