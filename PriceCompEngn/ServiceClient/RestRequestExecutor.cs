6e6bc669a90bc5909c06279f1518bde1b5d61873
Rytis Bankieta <rytis.bankieta@gmail.com>
2017-11-17 22:32:06 +0200

    Create classes for client side interaction with web service and test them

---
+++ PriceCompEngn/ServiceClient/RestRequestExecutor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace ServiceClient
+{
+    public class RestRequestExecutor
+    {
+        public async Task<string> ExecuteRestGetRequest(URIBuilder builder)
+        {
+            var client = new RestClient(builder.ServerAddress);
+
+            var request = new RestRequest(builder.Uri, Method.GET);
+
+            var asyncQueryResult = await client.ExecuteTaskAsync(request);
+
+            var result = asyncQueryResult.Content;
+
+            return result;
+        }
+
+        public async Task<string> ExecuteRestPostRequest(URIBuilder builder, byte[] image)
+        {
+            var client = new RestClient(builder.ServerAddress);
+
+            var request = new RestRequest(builder.Uri, Method.POST);
+            request.AddFile("image", image, "image.png");
+            request.AddHeader("Content-Type", "multipart/form-data");
+
+            var asyncQueryResult = await client.ExecuteTaskAsync(request);
+
+            var result = asyncQueryResult.Content;
+
+            return result;
+        }
+    }
+}
---
+++ PriceCompEngn/ServiceClientTest/URIBuilderTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceClient;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace ServiceClientTest
+{
+    [TestClass]
+    public class URIBuilderTest
+    {
+        [TestMethod]
+        public void URIBuilderFindsCorrectConfigurationSettings()
+        {
+            URIBuilder[] builders = new URIBuilder[5]
+            {
+                new URIBuilder(Resources.CheapestItems),
+                new URIBuilder(Resources.PriceComparator),
+                new URIBuilder(Resources.ShoppingCart),
+                new URIBuilder(Resources.TextManager),
+                new URIBuilder(Resources.TopItems)
+            };
+
+            string[] expectedUris = new string[5]
+            {
+                "/api/CheapestItems?",
+                "/api/PriceComparator?",
+                "/api/ShoppingCart?",
+                "/api/TextManager",
+                "/api/TopItems?"
+            };
+
+            for(int i = 0; i < 5; i++)
+            {
+                Assert.AreEqual(builders[i].Uri, expectedUris[i]);
+            }
+
+        }
+
+        [TestMethod]
+        public void URIBuilderAppendsArrays()
+        {
+            URIBuilder builder = new URIBuilder(Resources.ShoppingCart);
+
+            string[] items =
+            {
+                "monster",
+                "red bull",
+                "Mineralinis vanduo Vytautas"
+            };
+
+            string[] shops =
+            {
+                "iki",
+                "rimi",
+                "maxima",
+                "norfa",
+                "lidl"
+            };
+
+            builder.AppendArrayArgs("items", items);
+            builder.AppendArrayArgs("shops", shops);
+
+            string expectedUri = "/api/ShoppingCart?items=monster&items=red bull&items=Mineralinis vanduo Vytautas&shops=iki&shops=rimi&shops=maxima&shops=norfa&shops=lidl&";
+            string actualUri = builder.Uri;
+
+            Assert.AreEqual(expectedUri, actualUri);
+            Console.WriteLine(builder.ServerAddress);
+        }
+
+        [TestMethod]
+        public void URIBuilderAppendsNumericArguments()
+        {
+            URIBuilder builder = new URIBuilder(Resources.CheapestItems);
+
+            int rows = 5;
+            int days = 7;
+
+            Dictionary<string, int> arguments = new Dictionary<string, int>()
+            {
+                { "rows", rows },
+                { "days", days }
+            };
+
+            builder.AppendNumericArgs(arguments);
+
+            string expectedUri = "/api/CheapestItems?rows=5&days=7&";
+            string actualUri = builder.Uri;
+
+            Assert.AreEqual(expectedUri, actualUri);
+        }
+    }
+}
