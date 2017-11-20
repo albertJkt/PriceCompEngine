b0e6a5b7d9e4e8e264ed69f95b52ed9abbd990e2
Rytis Bankieta <rytis.bankieta@gmail.com>
2017-11-20 23:20:07 +0200

    Properly configure RestSharp so the damn program doesn't ask for useless System.Web.dll which completely breaks the entire project
    This is why Xamarin is the biggest peace of shit of a framework for mobile development, especially platform specific.

--- PriceCompEngn/PriceCompEngnMobile/Resources/Resource.Designer.cs
+++ PriceCompEngn/PriceCompEngnMobile/Resources/Resource.Designer.cs
@@ -63,38 +63,44 @@ namespace PriceCompEngnMobile
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050005
+			public const int checkBox3 = 2131034117;
+			
 			// aapt resource value: 0x7f050004
-			public const int checkBox3 = 2131034116;
+			public const int checkBox4 = 2131034116;
 			
 			// aapt resource value: 0x7f050003
-			public const int checkBox4 = 2131034115;
+			public const int checkBox5 = 2131034115;
 			
 			// aapt resource value: 0x7f050002
-			public const int checkBox5 = 2131034114;
+			public const int checkBox6 = 2131034114;
 			
 			// aapt resource value: 0x7f050001
-			public const int checkBox6 = 2131034113;
-			
-			// aapt resource value: 0x7f050000
-			public const int checkBox7 = 2131034112;
-			
-			// aapt resource value: 0x7f050006
-			public const int imageButton1 = 2131034118;
+			public const int checkBox7 = 2131034113;
 			
 			// aapt resource value: 0x7f050007
-			public const int list = 2131034119;
-			
-			// aapt resource value: 0x7f050005
-			public const int textView = 2131034117;
+			public const int imageButton1 = 2131034119;
 			
 			// aapt resource value: 0x7f050008
-			public const int text_name = 2131034120;
+			public const int list = 2131034120;
+			
+			// aapt resource value: 0x7f050000
+			public const int shop_cart_fragment_container = 2131034112;
 			
 			// aapt resource value: 0x7f050009
-			public const int text_price = 2131034121;
+			public const int test_text_view = 2131034121;
+			
+			// aapt resource value: 0x7f050006
+			public const int textView = 2131034118;
 			
 			// aapt resource value: 0x7f05000a
-			public const int text_type = 2131034122;
+			public const int text_name = 2131034122;
+			
+			// aapt resource value: 0x7f05000b
+			public const int text_price = 2131034123;
+			
+			// aapt resource value: 0x7f05000c
+			public const int text_type = 2131034124;
 			
 			static Id()
 			{
@@ -119,7 +125,10 @@ namespace PriceCompEngnMobile
 			public const int fragment_shop_cart_list = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int shop_cart_list_row = 2130903043;
+			public const int rest_test = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int shop_cart_list_row = 2130903044;
 			
 			static Layout()
 			{
