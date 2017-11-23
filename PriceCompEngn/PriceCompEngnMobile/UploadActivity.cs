a6843f8a41f95847d1b2c90ddfa9c2cb7e54d719
Rytis Bankieta <31857639+RytisBnk@users.noreply.github.com>
2017-11-24 00:02:40 +0200

    Android frontend dup merge into Android-frontend-continue (#49)
    * minor code adjustments
    
    * finish upload layout and its functionality
    
    * some adjustments to UI

--- PriceCompEngn/PriceCompEngnMobile/UploadActivity.cs
+++ PriceCompEngn/PriceCompEngnMobile/UploadActivity.cs
@@ -18,7 +18,8 @@ using Android.Graphics.Drawables;
 using Android.Media;
 using Newtonsoft.Json;
 using Models;
-
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace PriceCompEngnMobile{
     [Activity(Label = "UploadActivity")]
@@ -26,6 +27,7 @@ namespace PriceCompEngnMobile{
     {
         public static readonly int PickImageId = 1000;
         string response = String.Empty;
+        public static List<ShopItem> items;
         ImageView _imageView;
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -41,6 +43,40 @@ namespace PriceCompEngnMobile{
             _imageView = FindViewById<ImageView>(Resource.Id.imgView);
 
             addBtn.Click += ButtonOnClick;
+
+            validateBtn.Click += delegate {
+                if (items!=null)
+                {
+                    var intent = new Intent(this, typeof(ValidateActivity));
+                    StartActivity(intent);
+                }
+                else
+                {
+                    Toast.MakeText(this, "Items were not parsed yet", ToastLength.Short).Show();
+                }
+            };
+
+            uplBtn.Click +=delegate
+            {
+                PCEUriBuilder pub = new PCEUriBuilder(ServiceClient.Resources.ShopItems);
+                RestRequestExecutor executor = new RestRequestExecutor();
+                var json = JsonConvert.SerializeObject(items);
+
+                pub.AppendStringArgs(new Dictionary<string, string>()
+                {
+                    { "itemListJson", json }
+
+                });
+
+                executor.ExecuteRestPostRequest(pub);
+
+                Toast.MakeText(this, "Information was successfully uploaded into the Database!", ToastLength.Short).Show();
+
+                response = String.Empty;
+                items = null;
+                _imageView = null;
+                Finish();
+            };
         }
 
         void ButtonOnClick(object sender, EventArgs eventArgs)
@@ -57,27 +93,38 @@ namespace PriceCompEngnMobile{
 
                 _imageView.SetImageURI(uri);
 
-                PCEUriBuilder builder = new PCEUriBuilder(ServiceClient.Resources.OCR);
-                RestRequestExecutor executor = new RestRequestExecutor();
+                await GetOcrText();
 
-                BitmapDrawable bd = (BitmapDrawable)_imageView.Drawable;
-                Bitmap bitmap = bd.Bitmap;
-
-                byte[] bitmapData;
-                using (var stream = new MemoryStream())
-                {
-                    bitmap.Compress(Bitmap.CompressFormat.Png, 0, stream);
-                    bitmapData = stream.ToArray();
-                }
-                response = await executor.ExecuteRestPostRequest(builder, bitmapData);
-
-                PCEUriBuilder build = new PCEUriBuilder(ServiceClient.Resources.TextManager);
-               
-                RestRequestExecutor exc = new RestRequestExecutor();
-                string json = await exc.ExecuteRestPostRequest(build,response);
-
-                List<ShopItem> items = JsonConvert.DeserializeObject<List<ShopItem>>(json);
+                await GetItemList();
+                Thread.Sleep(500);
             }  
         } 
+
+        private async Task GetOcrText()
+        {
+            PCEUriBuilder builder = new PCEUriBuilder(ServiceClient.Resources.OCR);
+            RestRequestExecutor executor = new RestRequestExecutor();
+
+            BitmapDrawable bd = (BitmapDrawable)_imageView.Drawable;
+            Bitmap bitmap = bd.Bitmap;
+
+            byte[] bitmapData;
+            using (var stream = new MemoryStream())
+            {
+                bitmap.Compress(Bitmap.CompressFormat.Png, 0, stream);
+                bitmapData = stream.ToArray();
+            }
+            response = await executor.ExecuteRestPostRequest(builder, bitmapData);
+        }
+
+        private async Task GetItemList()
+        {
+            PCEUriBuilder build = new PCEUriBuilder(ServiceClient.Resources.TextManager);
+
+            RestRequestExecutor exc = new RestRequestExecutor();
+            string json = await exc.ExecuteRestPostRequest(build, response);
+
+            items = JsonConvert.DeserializeObject<List<ShopItem>>(json);
+        }
     }
 }
---
+++ PriceCompEngn/PriceCompEngnMobile/ValidateActivity.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PriceCompEngnMobile
+{
+    [Activity(Label = "ValidateActivity")]
+    public class ValidateActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.validate);
+
+            string previous=String.Empty;
+            string result;
+            var text = FindViewById<EditText>(Resource.Id.textVw);
+            var validate = FindViewById<ImageButton>(Resource.Id.validateBtn);
+
+            // get string before validation
+            if(UploadActivity.items!=null){
+                foreach (var item in UploadActivity.items)
+                {
+                    string temp = item.Type + " " + '"' + item.ItemName + '"' + " " + item.Price + " eu\n";
+                    previous = String.Concat(previous, temp);
+                    temp = String.Empty;
+                }
+                previous = TextManager.DeleteLines(previous, 1, true);
+            }
+                        
+            DisplayItems(text);
+
+            validate.Click+=delegate {
+
+                result = TextManager.RemoveEndlines(text.Text);
+
+                string pattern = " ?\" ?| *eu *";
+
+                string[] substrings = Regex.Split(result, pattern);
+
+                for (int i = 0; i < (substrings.Length / 3); i++)
+                {
+                    UploadActivity.items[i].Type = substrings[i * 3];
+                    UploadActivity.items[i].ItemName = substrings[i * 3 +1];
+                    UploadActivity.items[i].Price = float.Parse(substrings[i * 3 + 2]);
+                }
+
+                Thread.Sleep(500);
+
+                Finish();
+
+            };
+
+
+
+        }
+
+        void DisplayItems(EditText x)
+        {
+            if (UploadActivity.items != null)
+            {
+                foreach (var item in UploadActivity.items)
+                {
+                    x.Append(item.Type + " " +'"' +item.ItemName +'"' + " " + item.Price + " eu\n");
+                }
+
+                x.Text = TextManager.DeleteLines(x.Text, 1, true);
+            }
+        }
+    }
+}
