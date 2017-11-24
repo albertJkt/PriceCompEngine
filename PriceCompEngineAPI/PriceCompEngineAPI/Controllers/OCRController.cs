6a99fa6fc5a500151eb15aec947b715444c42895
Rytis Bankieta <rytis.bankieta@gmail.com>
2017-11-24 14:57:23 +0200

    Update code in web service solution to match what is already deployed on Azure

---
+++ PriceCompEngineAPI/PriceCompEngineAPI/Controllers/OCRController.cs
@@ -0,0 +1,40 @@
+﻿using DataBase;
+using OCREngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Logic;
+using System.Diagnostics;
+using System.IO;
+using System.Drawing;
+using System.Web.Hosting;
+using System.Threading.Tasks;
+using System.Web;
+using PriceCompEngineAPI.Extensions;
+
+namespace PriceCompEngineAPI.Controllers
+{
+    public class OCRController : ApiController
+    {
+        [HttpPost]
+        public async Task<string> Post(HttpRequestMessage request)
+        {
+            if (!request.Content.IsMimeMultipartContent())
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+
+            var data = await request.Content.ParseMultipartAsync();
+
+            if (data.Files.ContainsKey("image"))
+            {
+                var file = data.Files["image"].File;
+                string imageText = OCREngineAPI.GetImageText(file, "lt", ResultFormat.TEXT);
+
+                return imageText;
+            }
+            else return null;
+        }       
+    }
+}
