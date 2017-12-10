e9e883918bd3fcd19de940adab28ad8884261acd
Rytis Bankieta <31857639+RytisBnk@users.noreply.github.com>
2017-12-10 16:45:14 +0200

    Shop check analyzing (#56)
    * Update TextManager controller to use shop specific classes
    
    * Added class for iki checks (#55)
    
    * Added class for iki checks
    
    Added a class that reads IKI checks
    
    * Linked to factory method

---
+++ PriceCompEngineAPI/Logic/ShopCheckAnalyzer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public abstract class ShopCheckAnalyzer
+    {
+        public List<string> Prices { get; protected set; }
+        public List<string> PayedPrices { get; protected set; }
+        public List<string> ItemNames { get; protected set; }
+        public string ShopName { get; protected set; }
+        public DateTime PurchaseTime { get; protected set; }
+
+        public abstract void AnalyzeText(string text);
+    }
+}
---
+++ PriceCompEngineAPI/Logic/ShopCheckAnalyzerCreator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Logic
+{
+    public class ShopCheckAnalyzerCreator
+    {
+        public ShopCheckAnalyzer Create(string text)
+        {
+            if (Regex.IsMatch(text, @"[Rr][Ii][Mm][Ii]"))
+            {
+                ShopCheckAnalyzer analyzer = new RimiCheckAnalyzer();
+                analyzer.AnalyzeText(text);
+                return analyzer;
+            }
+
+            else if (Regex.IsMatch(text, @"[Pp][Aa][Ll][Ii][Nn][Kk]"))
+            {
+                ShopCheckAnalyzer analyzer = new IkiCheckAnalyzer();
+                analyzer.AnalyzeText(text);
+                return analyzer;
+            }
+
+            else throw new ArgumentException("Text doesn't match any shop signature");
+        } 
+    }
+}
