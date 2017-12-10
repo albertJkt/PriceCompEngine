37ff2684e067344f07d13109586394fc0f99acc9
Albert Jurkoit <31667609+albertJkt@users.noreply.github.com>
2017-12-10 16:46:35 +0200

    Shop check analyzing (#56) (#57)
    * Update TextManager controller to use shop specific classes
    
    * Added class for iki checks (#55)
    
    * Added class for iki checks
    
    Added a class that reads IKI checks
    
    * Linked to factory method

---
+++ PriceCompEngineAPI/Logic/IkiCheckAnalyzer.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class IkiCheckAnalyzer : ShopCheckAnalyzer
+    {
+        private List<string> _itemNames = new List<string>();
+        private List<string> _prices = new List<string>();
+        private List<string> _receiptList = new List<string>();
+        private string _purchaseDate;
+        private string _purchaseTime;
+
+
+        /*patterns*/
+        private string _pricePattern = @"^(\d+(?:[.,]\d{2})\s[A].?)$";
+        private string _discountPattern = @"^(-\d+(?:[.,]\d{2})\s[A].?)$";
+        private string _datePattern = @"[0-9]{4}-[0-1][0-9]-[0-3][0-9]";
+        private string _timePattern = @"[0-9]{2}:[0-9]{2}:[0-9]{2}";
+        private string _unitpricePattern = @"^(\d+(?:[.,]\d{3})\s[Kk][Gg]\s[Xx]\s\d+(?:[.,]\d{2})\s[Ee][Uu][Rr]/[Kk][Gg])$";
+
+
+
+        public override void AnalyzeText(string receiptText)
+        {
+
+            _receiptList = TransformText(receiptText);
+            ParseReceiptList(_receiptList);
+
+            PayedPrices = _prices;
+            ItemNames = _itemNames;
+            PurchaseTime = Convert.ToDateTime((Regex.Replace(_purchaseDate, "[A-Za-z ]", String.Empty))
+                + " " + (Regex.Replace(_purchaseTime, "[A-Za-z ]", String.Empty)));
+
+            Prices = new List<string>(_prices);
+        }
+
+        private List<string> TransformText(string receiptText)
+        {
+            receiptText = receiptText.Substring(0, receiptText.IndexOf(@"\nSUMA\n"));
+            var receiptList = new List<string>
+                (receiptText.Split(
+                    new string[] { "\\n" },
+                    StringSplitOptions.RemoveEmptyEntries)
+                    );
+
+            receiptList = receiptList.Select(s => s.Replace(',', '.')).ToList();
+            receiptList = receiptList.Select(s => s.Replace("\\", "")).ToList();
+
+            return receiptList;
+        }
+
+        private void ParseReceiptList(List<string> receiptList)
+        {
+            for (int i = 0; i < receiptList.Count(); i++)
+            {
+                if (Regex.IsMatch(receiptList[i], _unitpricePattern))
+                {
+                    receiptList.RemoveAt(i);
+                }
+
+                if (Regex.IsMatch(receiptList[i], _datePattern))
+                {
+                    _purchaseDate = receiptList[i];
+                    continue;
+                }
+
+                if (Regex.IsMatch(receiptList[i], _timePattern))
+                {
+                    _purchaseTime = receiptList[i];
+                    continue;
+                }
+
+                if (Regex.IsMatch(receiptList[i], _discountPattern))
+                {
+                    receiptList[i] = receiptList[i].Replace(" A", String.Empty);
+
+                    var temp = (Convert.ToDecimal(receiptList[i]) +
+                        Convert.ToDecimal(_prices.Last())).ToString();
+
+                    _prices.RemoveAt(_prices.Count() - 1);
+                    _prices.Add(temp);
+                    continue;
+                }
+
+                if (Regex.IsMatch(receiptList[i], _pricePattern))
+                {
+                    if (receiptList[i].Contains(" A."))
+                    {
+                        receiptList[i] = receiptList[i].Replace(" A.", String.Empty);
+                    }
+                    else receiptList[i] = receiptList[i].Replace(" A", String.Empty);
+
+                    _prices.Add(receiptList[i]);
+                    _itemNames.Add(receiptList[i - 1]);
+                    continue;
+                }
+            }
+        }
+    }
+}
+
---
+++ PriceCompEngineAPI/Logic/RimiCheckAnalyzer.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class RimiCheckAnalyzer : ShopCheckAnalyzer
+    {
+        private string _receiptText;
+        private MatchCollection _priceMatches;
+
+        public override void AnalyzeText(string text)
+        {
+            _receiptText = text;
+            ItemNames = GetItemNames();
+            ShopName = "Rimi";
+            PurchaseTime = DateTime.UtcNow;
+            StandartisePrices();
+            CorrectPerWeightPrices();
+            CorrectPerUnitPrices();
+        }
+
+        private List<string> GetItemNames()
+        {
+            _receiptText = CutBeginning();
+            List<string> prices = GetPrices();
+            PayedPrices = prices;
+            Prices = new List<string>(prices);
+            List<string> items = new List<string>();
+            for (int i = 0; i < _priceMatches.Count; i++)
+            {
+                string item;
+                if (i == 0)
+                {
+                    item = new string(_receiptText.Take(_priceMatches[i].Index).ToArray());
+                    item = item.Replace("\n", " ");
+                    _receiptText = _receiptText.Remove(0, _priceMatches[i].Index);
+                }
+                else
+                {
+                    item = new string(_receiptText.Take(_priceMatches[i].Index - _priceMatches[i - 1].Index - 7).ToArray());
+                    item = item.Replace("\n", " ");
+                    _receiptText = _receiptText.Remove(0, _priceMatches[i].Index - _priceMatches[i - 1].Index - 7);
+                }
+
+                items.Add(item);
+                _receiptText = _receiptText.Remove(0, 7);
+            }
+
+            CorrectDiscount(items);
+
+            return items;
+        }
+
+        private void CorrectPerUnitPrices()
+        {
+            string pattern1 = @"[0-9]\s*vnt.\s*[Xx]\s*[0-9]+[.,][0-9]{2}\s*EUR";
+            string pattern2 = @"[0-9]+[.,][0-9]+";
+
+            CorrectNameAndPrice(pattern1, pattern2);
+        }
+
+        private void CorrectPerWeightPrices()
+        {
+            string pattern1 = @"[1il]\s*kg\s+[0-9]+[.,][0-9]+\s*kg\s*[Xx]\s*[0-9]+[.,][0-9]+\s*EUR/kg";
+            string pattern2 = @"[0-9]+[.,][0-9]+";
+
+            CorrectNameAndPrice(pattern1, pattern2);
+        }
+
+        private void CorrectNameAndPrice(string pattern1, string pattern2)
+        {
+            for (int i = 0; i < ItemNames.Count; i++)
+            {
+                if (Regex.IsMatch(ItemNames[i], pattern1))
+                {
+                    var match = Regex.Match(ItemNames[i], pattern1);
+                    var matchText = match.Value;
+
+                    var newPrice = Regex.Matches(matchText, pattern2)
+                                        .Cast<Match>()
+                                        .OrderByDescending(m => m.Index)
+                                        .First()
+                                        .Value;
+                    Prices[i] = newPrice;
+
+                    ItemNames[i] = ItemNames[i].Remove(match.Index);
+                }
+            }
+        }
+
+        private void StandartisePrices()
+        {
+            for (int i = 0; i < Prices.Count; i++)
+            {
+                Prices[i] = Prices[i].Replace(",", ".")
+                                     .Remove(Prices[i].Count() - 2);
+            }
+        }
+
+        private string CorrectDiscount(List<string> items)
+        {
+            string discountPattern = @"Nuol.\s+-[0-9]+[.,][0-9]{2}\s+Galut.\s+kaina\s+[0-9]+[.,][0-9]{2}";
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (Regex.IsMatch(items[i], discountPattern))
+                {
+                    var matchCollection = Regex.Matches(items[i], discountPattern).Cast<Match>();
+
+                    var match = matchCollection.First();
+
+                    var matchText = matchCollection.Select(m => m.Value).First();
+
+                    var newPrice = Regex.Matches(matchText, @"[0-9]+[.,][0-9]{2}")
+                                        .Cast<Match>()
+                                        .Select(m => m.Value)
+                                        .ElementAt(1)
+                                        .Replace(",", ".");
+
+                    Prices[i - 1] = newPrice + " A";
+
+                    items[i] = items[i].Remove(match.Index, matchText.Count() + 1);
+                }
+            }
+
+            return null;
+        }
+
+        private string GetPurchaseTime()
+        {
+            string pattern = @"20[0-9]{2}-[0-1][0-9]-[0-3][0-9]\s*[0-2][0-9]:[0-5][0-9]:[0-5][0-9]";
+
+            string dateTime = Regex.Match(_receiptText, pattern).Value;
+
+            return dateTime;
+        }
+
+        private string CutBeginning()
+        {
+            var pattern1 = @"X+[0-9]{4}";
+            var pattern2 = @"LT[0-9]{9}";
+
+            if (Regex.IsMatch(_receiptText, pattern1))
+            {
+                var match = Regex.Match(_receiptText, pattern1);
+                _receiptText = _receiptText.Remove(0, match.Index);
+                _receiptText = _receiptText.Replace(match.Value, string.Empty);
+            }
+            else if (Regex.IsMatch(_receiptText, pattern2))
+            {
+                var match = Regex.Match(_receiptText, pattern2);
+                _receiptText = _receiptText.Remove(0, match.Index);
+                _receiptText = _receiptText.Replace(match.Value, string.Empty);
+            }
+            else return _receiptText;
+
+            _receiptText = _receiptText.Remove(0, 1);
+
+            return _receiptText;
+        }
+
+        private List<string> GetPrices()
+        {
+            string pattern = @"[0-9]*[.,][0-9]{2}\s*[AE][\n.]";
+
+            _priceMatches = Regex.Matches(_receiptText, pattern);
+
+            var matches = _priceMatches
+                                .Cast<Match>()
+                                .Select(m => m.Value)
+                                .ToList();
+
+            for (int i = 0; i < matches.Count(); i++)
+            {
+                matches[i] = matches[i].Remove(matches[i].Count() - 1);
+            }
+
+            return matches;
+        }
+    }
+}
