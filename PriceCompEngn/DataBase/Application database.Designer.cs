e4c388194bba112fd37375573f4fe46b08d526b0
Albert Jurkoit <31667609+albertJkt@users.noreply.github.com>
2017-12-10 16:56:06 +0200

    Update EF mode (#58)
    * Shop check analyzing (#56) (#57)
    
    * Update TextManager controller to use shop specific classes
    
    * Added class for iki checks (#55)
    
    * Added class for iki checks
    
    Added a class that reads IKI checks
    
    * Linked to factory method
    
    * update EF model

--- PriceCompEngn/DataBase/Application database.Designer.cs
+++ PriceCompEngn/DataBase/Application database.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\KaSaras\Documents\GitHub\PriceCompEngine\PriceCompEngn\DataBase\Application database.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Albert\Documents\GitHub\PriceCompEngine\PriceCompEngn\DataBase\Application database.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
