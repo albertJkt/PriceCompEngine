de422cbd6342216485562db169562e01026b9707
Albert Jurkoit <31667609+albertJkt@users.noreply.github.com>
2017-12-12 22:57:42 +0200

    modify db controller and user controller

--- PriceCompEngineAPI/PriceCompEngineAPI/Controllers/UserController.cs
+++ PriceCompEngineAPI/PriceCompEngineAPI/Controllers/UserController.cs
@@ -16,11 +16,40 @@ namespace PriceCompEngineAPI.Controllers
             db.InsertUser(newUser);
         }
         
-        public List<User> Get ()
+
+        /// <summary>
+        /// Get the specified user.
+        /// </summary>
+        /// <returns>User object if user tries to login and it's successful
+        /// Null - if user doesn't exist or username (and/or email) is taken
+        /// User object with password 1 if username and email is not taken</returns>
+        public User Get([FromUri] User user)
         {
-            DBController db = new DBController();
-            List<User> users = db.GetUsers();
-            return users;
+            if (string.IsNullOrEmpty(user.Email))
+            {
+                DBController db = new DBController();
+                User usr = db.GetUser(user.UserName, user.Password);
+                if (usr != null)
+                {
+                    return usr;
+                }
+                else return null;
+            }
+            else
+            {
+                DBController db = new DBController();
+                bool Exists = db.CheckIfExists(user.UserName, user.Email);
+                if (Exists)
+                {
+                    return null;
+                }
+                else
+                {
+                    User useris = new User();
+                    useris.Password = "1";
+                    return useris;
+                }
+            }
         }
     }
 }
