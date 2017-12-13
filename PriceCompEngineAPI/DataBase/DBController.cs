13f130e025b26ed7bd6755ef9303e9c475b9049e
Albert Jurkoit <31667609+albertJkt@users.noreply.github.com>
2017-12-13 02:16:01 +0200

    Update DBController.cs

--- PriceCompEngineAPI/DataBase/DBController.cs
+++ PriceCompEngineAPI/DataBase/DBController.cs
@@ -167,7 +167,28 @@ namespace DataBase
 
             return users;
         }
-       
+        
+        public User GetUser(string username, string password)
+        {
+            using (var context = new PriceCompEngineEntities())
+            {
+                IQueryable<User> query = from user in context.Users
+                                         where user.UserName == username
+                                         where user.Password == password
+                                         select user;
 
+                User usr;
+                try
+                {
+                    usr = query.Single();
+                    return usr;
+                }
+                catch (InvalidOperationException ex)
+                {
+                    return null;
+                }
+            }
+        }
+       
     }
 }
